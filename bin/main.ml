0ade4b43cb4c7b1c01c7d0eb005da2c49d2565ef
Emmanuel Suarez Acevedo <19786004+emmanueljs1@users.noreply.github.com>
2022-01-01 17:59:54 -0400

    Initial CPU loop and main class

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,18 @@
-let () = print_endline "Hello, World!"
+open Chip8
+
+let () =
+  let args = Sys.argv in
+  let debug = Array.exists (fun s -> s = "--debug") args in
+  let frequency =
+    let fallback = 600 in
+    match Array.find_opt (fun s -> String.starts_with ~prefix:"--frequency" s) args with
+    | None -> fallback
+    | Some(s) ->
+        begin match String.split_on_char '=' s with
+        | ["--frequency"; freq_str] ->
+            begin try int_of_string freq_str with _ -> fallback end
+        | _ -> fallback
+        end
+  in
+  let program = char_of_int 0x00E0 |> Array.make 100 in
+  Cpu.run_with ~debug:debug ~freq:frequency ~prog:program
--- lib/cpu.ml
+++ lib/cpu.ml
@@ -20,32 +20,134 @@ type registers =
 type instruction =
   | ClearScreen
 
-let decode_instruction (_: char) : instruction =
+let decode_instruction (_: char) : instruction * int =
   failwith "uninplemented"
 
 type cpu =
-  { memory: char array
+  { mem: char array
   ; stack: int list
   ; display: bool array
   ; sound_timer: char
   ; delay_timer: char
   ; index: int
   ; pc: int
+  ; off: bool
   }
 
-let execute_instruction : cpu -> instruction -> cpu =
+let needs_timer_update (cpu: cpu) : bool =
+  int_of_char cpu.sound_timer > 0 || int_of_char cpu.delay_timer > 0
+
+let decr_timers (cpu: cpu) : cpu =
+  let sound_timer' = int_of_char cpu.sound_timer - 1 |> max 0 |> char_of_int in
+  let delay_timer' = int_of_char cpu.delay_timer - 1 |> max 0 |> char_of_int in
+  { cpu with sound_timer = sound_timer' ; delay_timer = delay_timer' }
+
+let execute_instruction (_: cpu) (_: instruction) : cpu =
   failwith "unimplemented"
 
-let init_cpu () : cpu =
-  { memory = char_of_int 0 |> Array.make 4096
+let boot ~program:(m: char array): cpu =
+  let load_font (_: char array) : unit =
+    failwith "unimplemented"
+  in
+
+  let mem = char_of_int 0 |> Array.make 4096 in
+  load_font mem;
+  Array.blit m 0 mem 0x0200 (Array.length m);
+
+  { mem = mem
   ; stack = []
   ; display = Array.make 2048 false
   ; sound_timer = char_of_int 0
   ; delay_timer = char_of_int 0
   ; index = 0
   ; pc = 0
+  ; off = false
   }
 
-let step (cpu: cpu) : cpu =
-  let encoded_instruction = Array.get (cpu.memory) cpu.pc in
-  encoded_instruction |> decode_instruction |> execute_instruction cpu
+let step (cpu: cpu) : cpu * int =
+  let encoded_instruction = cpu.mem.(cpu.pc) in
+  let instruction, duration = decode_instruction encoded_instruction in
+  let cpu' = execute_instruction cpu instruction in
+  cpu', duration
+
+let run_with ~debug:(_: bool) ~freq:(hz: int) ~prog:(m: char array) : unit =
+  let cycle_time = 1. /. (float_of_int hz) in
+  let timer_update_time = 1. /. 60. in
+
+  let cpu = boot ~program:m |> ref in
+  let booted_at = Unix.gettimeofday () in
+  let cpu_mutex = Mutex.create () in
+
+  let timer_update_thread =
+    Thread.create (fun init ->
+      let rec timer_update_loop (last_timer_update: float) : unit =
+        let updated_at = Unix.gettimeofday () in
+
+        Mutex.lock cpu_mutex;
+        if !cpu.off then Thread.exit ();
+        Mutex.unlock cpu_mutex;
+
+        let timer_updates =
+          (updated_at -. last_timer_update) /. timer_update_time
+        in
+        for _ = 1 to ceil timer_updates |> int_of_float do
+          Mutex.lock cpu_mutex;
+          if needs_timer_update !cpu then cpu := decr_timers !cpu;
+          Mutex.unlock cpu_mutex;
+        done;
+
+        let now = Unix.gettimeofday () in
+        let elapsed = now -. updated_at in
+        if elapsed < timer_update_time then
+          timer_update_time -. elapsed |> Thread.delay;
+
+        timer_update_loop updated_at
+      in
+      timer_update_loop init
+    ) booted_at
+  in
+
+  let cycle_thread =
+    Thread.create (fun init ->
+      let rec clock_loop (last_clock_tick: float) : unit =
+        let tick = Unix.gettimeofday () in
+
+        (* TODO: turn off cpu and exit thread based on user input *)
+
+        let cycles_to_run =
+          (tick -. last_clock_tick) /. cycle_time |> int_of_float
+        in
+        let rec loop (n: int) (time_elapsed_in_cycle: float) : unit =
+          begin match n with
+          | 0 -> ()
+          | _ ->
+            Mutex.lock cpu_mutex;
+            let (cpu', duration_ms) = step !cpu in
+            cpu := cpu';
+            Mutex.unlock cpu_mutex;
+            let time_elapsed_in_cycle' =
+              time_elapsed_in_cycle +. (duration_ms / 1000 |> float_of_int)
+            in
+            let cycles_ran =
+              time_elapsed_in_cycle' /. cycle_time |> int_of_float
+            in
+            let time_elapsed_in_cycle'' =
+              if cycles_ran > 0 then time_elapsed_in_cycle' else 0.
+            in
+            loop (n - cycles_ran) time_elapsed_in_cycle''
+          end
+        in
+        loop cycles_to_run 0.;
+
+        let now = Unix.gettimeofday () in
+        let elapsed = now -. tick in
+        if elapsed < cycle_time then cycle_time -. elapsed |> Thread.delay;
+
+        clock_loop tick
+      in
+      clock_loop init
+    ) booted_at
+  in
+
+  Thread.join cycle_thread;
+  Thread.join timer_update_thread
