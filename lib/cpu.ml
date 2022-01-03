cf208e7d3a95c9fc9bca57c3abfbf96b3cb62ebc
Emmanuel Suarez Acevedo <19786004+emmanueljs1@users.noreply.github.com>
2022-01-03 11:50:46 -0400

    Add CPU state, further stub execute instruction

--- lib/cpu.ml
+++ lib/cpu.ml
@@ -20,6 +20,12 @@ type registers =
 type instruction =
   | ClearScreen
 
+type state =
+  | Running
+  | Booted
+  | Paused
+  | Off
+
 let decode_instruction (_: char) : instruction * float =
   (* TODO: implement *)
   (ClearScreen,  1000.)
@@ -32,7 +38,7 @@ type cpu =
   ; delay_timer: char
   ; index: int
   ; pc: int
-  ; off: bool
+  ; state: state
   }
 
 let needs_timer_update (cpu: cpu) : bool =
@@ -43,9 +49,16 @@ let decr_timers (cpu: cpu) : cpu =
   let delay_timer' = int_of_char cpu.delay_timer - 1 |> max 0 |> char_of_int in
   { cpu with sound_timer = sound_timer' ; delay_timer = delay_timer' }
 
-let execute_instruction (cpu: cpu) (_: instruction) : cpu =
-  (* TODO: implement *)
-  { cpu with pc = cpu.pc + 1 }
+let execute_instruction (cpu: cpu) (ins: instruction) : cpu =
+  let next_pc (_: instruction) : int =
+    (* TODO: implement *)
+    cpu.pc + 1
+  in
+  let cpu' =
+    (* TODO: execute instruction *)
+    cpu
+  in
+  { cpu' with pc = next_pc ins }
 
 let boot ~program:(m: char array): cpu =
   let load_font (_: char array) : unit =
@@ -64,7 +77,7 @@ let boot ~program:(m: char array): cpu =
   ; delay_timer = char_of_int 0
   ; index = 0
   ; pc = 0
-  ; off = false
+  ; state = Booted
   }
 
 let step (cpu: cpu) : cpu * float =
@@ -85,29 +98,39 @@ let run_with ~debug:(_: bool) ~freq:(hz: int) ~prog:(m: char array) : unit =
     Thread.create (fun init ->
       let rec timer_loop last_timer_tick =
         let now = Unix.gettimeofday () in
-
-        (* TODO: exit thread if CPU is off *)
-
         let elapsed_since_last_tick = now -. last_timer_tick in
 
-         if elapsed_since_last_tick < timer_update_time then (
-           timer_update_time -. elapsed_since_last_tick |> Thread.delay;
-           timer_loop last_timer_tick
-         ) else
-          let timer_updates =
-            elapsed_since_last_tick /. timer_update_time |> int_of_float
-          in
+        match !cpu.state with
+        | Running ->
+           if elapsed_since_last_tick < timer_update_time then (
+             timer_update_time -. elapsed_since_last_tick |> Thread.delay;
+             timer_loop last_timer_tick
+           ) else
+            let timer_updates =
+              elapsed_since_last_tick /. timer_update_time |> int_of_float
+            in
 
-          for _ = 1 to timer_updates do
-            Mutex.lock cpu_mutex;
-            if needs_timer_update !cpu then cpu := decr_timers !cpu;
-            Mutex.unlock cpu_mutex;
-          done;
+            for _ = 1 to timer_updates do
+              Mutex.lock cpu_mutex;
+              if needs_timer_update !cpu then cpu := decr_timers !cpu;
+              Mutex.unlock cpu_mutex;
+            done;
 
-          let elapsed_in_updates_calc =
-            float_of_int timer_updates *. timer_update_time
-          in
-          timer_loop (last_timer_tick +. elapsed_in_updates_calc)
+            let elapsed_in_updates_calc =
+              float_of_int timer_updates *. timer_update_time
+            in
+            timer_loop (last_timer_tick +. elapsed_in_updates_calc)
+        | Paused ->
+            if elapsed_since_last_tick > timer_update_time then
+              let noop_updates =
+                elapsed_since_last_tick /. timer_update_time |> int_of_float
+              in
+              let noop_updates_elapsed =
+                float_of_int noop_updates *. timer_update_time
+              in
+              timer_loop (now +. noop_updates_elapsed);
+        | Booted -> timer_loop last_timer_tick
+        | Off -> Thread.exit ()
       in
       timer_loop init
     ) booted_at
@@ -117,64 +140,78 @@ let run_with ~debug:(_: bool) ~freq:(hz: int) ~prog:(m: char array) : unit =
     Thread.create (fun init ->
       let rec clock_loop last_clock_tick =
         let now = Unix.gettimeofday () in
-
-        (* TODO: turn off cpu and exit thread based on user input *)
-
         let elapsed_since_last_tick = now -. last_clock_tick in
 
-        if elapsed_since_last_tick < cycle_time then (
-          let cycles_to_sleep = float_of_int hz /. 50. |> max 10. in
-          (cycle_time *. cycles_to_sleep) -. elapsed_since_last_tick
-          |> Thread.delay;
-          clock_loop last_clock_tick
-        ) else
-          let cycles_to_run =
-            elapsed_since_last_tick /. cycle_time |> int_of_float
-          in
+        match !cpu.state with
+        | Running ->
+          if elapsed_since_last_tick < cycle_time then (
+            let cycles_to_sleep = float_of_int hz /. 50. |> max 10. in
+            (cycle_time *. cycles_to_sleep) -. elapsed_since_last_tick
+            |> Thread.delay;
+            clock_loop last_clock_tick
+          ) else
+            let cycles_to_run =
+              elapsed_since_last_tick /. cycle_time |> int_of_float
+            in
 
-          let rec loop n time_elapsed_in_cycle =
-            if n > 0 then (
-              Mutex.lock cpu_mutex;
-              let (cpu', duration_ms) = step !cpu in
-              cpu := cpu';
-              Mutex.unlock cpu_mutex;
+            let rec loop n time_elapsed_in_cycle =
+              if n > 0 then (
+                Mutex.lock cpu_mutex;
+                let (cpu', duration_ms) = step !cpu in
+                cpu := cpu';
+                Mutex.unlock cpu_mutex;
 
-              let time_elapsed_in_cycle' =
-                time_elapsed_in_cycle +. (duration_ms /. 1_000_000.)
-              in
-
-              if time_elapsed_in_cycle' > cycle_time then (
-                let extra_cycles_time = time_elapsed_in_cycle' -. cycle_time in
-                let extra_cycles =
-                  extra_cycles_time /. cycle_time |> ceil |> int_of_float
-                in
-                let time_elapsed_in_next_cycle =
-                  extra_cycles_time -. (float_of_int extra_cycles *. cycle_time)
+                let time_elapsed_in_cycle' =
+                  time_elapsed_in_cycle +. (duration_ms /. 1_000_000.)
                 in
 
-                if extra_cycles <= n - 1 then
-                  loop (n - 1 - extra_cycles) time_elapsed_in_next_cycle
+                if time_elapsed_in_cycle' > cycle_time then (
+                  let extra_cycles_time = time_elapsed_in_cycle' -. cycle_time in
+                  let extra_cycles =
+                    extra_cycles_time /. cycle_time |> ceil |> int_of_float
+                  in
+                  let time_elapsed_in_next_cycle =
+                    extra_cycles_time -. (float_of_int extra_cycles *. cycle_time)
+                  in
+
+                  if extra_cycles <= n - 1 then
+                    loop (n - 1 - extra_cycles) time_elapsed_in_next_cycle
+                  else
+                    extra_cycles
+                ) else if time_elapsed_in_cycle' = cycle_time then
+                  loop (n - 1) 0.
                 else
-                  extra_cycles
-              ) else if time_elapsed_in_cycle' = cycle_time then
-                loop (n - 1) 0.
+                  loop n time_elapsed_in_cycle'
+              ) else if time_elapsed_in_cycle > 0. then
+                1
               else
-                loop n time_elapsed_in_cycle'
-            ) else if time_elapsed_in_cycle > 0. then
-              1
-            else
-              0
-          in
+                0
+            in
 
-          let extra_cycles = loop cycles_to_run 0. in
-          let elapsed_in_cycles =
-            float_of_int (cycles_to_run + extra_cycles) *. cycle_time
-          in
-          clock_loop (last_clock_tick +. elapsed_in_cycles)
+            let extra_cycles = loop cycles_to_run 0. in
+            let elapsed_in_cycles =
+              float_of_int (cycles_to_run + extra_cycles) *. cycle_time
+            in
+            clock_loop (last_clock_tick +. elapsed_in_cycles)
+        | Paused ->
+            if elapsed_since_last_tick > cycle_time then
+              let noop_cycles =
+                elapsed_since_last_tick /. cycle_time |> int_of_float
+              in
+              let noop_cycles_elapsed =
+                float_of_int noop_cycles *. cycle_time
+              in
+              clock_loop (now +. noop_cycles_elapsed)
+            else
+              clock_loop last_clock_tick
+        | Booted -> clock_loop last_clock_tick
+        | Off -> Thread.exit ()
       in
       clock_loop init
     ) booted_at
   in
+
+  (* TODO: user input thread to turn off CPU *)
 
   Thread.join clock_thread;
   Thread.join timer_thread
