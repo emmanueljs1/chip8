3d558182afb3b4bd1023141c0fc8ba9791d1f9c9
Emmanuel Suarez Acevedo <19786004+emmanueljs1@users.noreply.github.com>
2022-01-06 18:13:00 -0400

    Line spacing

--- lib/cpu.ml
+++ lib/cpu.ml
@@ -135,55 +135,58 @@ module Make (GUI: Gui.GUI) = struct
 
           match !cpu.state with
           | Running ->
-            if elapsed_since_last_tick < cycle_time then (
-              let cycles_to_sleep = float_of_int frequency /. 50. |> max 10. in
-              (cycle_time *. cycles_to_sleep) -. elapsed_since_last_tick
-              |> Thread.delay;
-              clock_loop last_clock_tick
-            ) else
-              let cycles_to_run =
-                elapsed_since_last_tick /. cycle_time |> int_of_float
-              in
+              if elapsed_since_last_tick < cycle_time then (
+                let cycles_to_sleep = float_of_int frequency /. 50. |> max 10. in
+                (cycle_time *. cycles_to_sleep) -. elapsed_since_last_tick
+                |> Thread.delay;
+                clock_loop last_clock_tick
+              ) else
+                let cycles_to_run =
+                  elapsed_since_last_tick /. cycle_time |> int_of_float
+                in
 
-              let rec loop n time_elapsed_in_cycle =
-                if n > 0 then (
-                  Mutex.lock cpu_mutex;
-                  let (cpu', duration_ms) = step !cpu in
-                  cpu := cpu';
-                  Mutex.unlock cpu_mutex;
+                let rec loop n time_elapsed_in_cycle =
+                  if n > 0 then (
+                    Mutex.lock cpu_mutex;
+                    let (cpu', duration_ms) = step !cpu in
+                    cpu := cpu';
+                    Mutex.unlock cpu_mutex;
 
-                  let time_elapsed_in_cycle' =
-                    time_elapsed_in_cycle +. (duration_ms /. 1_000_000.)
-                  in
-
-                  if time_elapsed_in_cycle' > cycle_time then (
-                    let extra_cycles_time = time_elapsed_in_cycle' -. cycle_time in
-                    let extra_cycles =
-                      extra_cycles_time /. cycle_time |> ceil |> int_of_float
-                    in
-                    let time_elapsed_in_next_cycle =
-                      extra_cycles_time -. (float_of_int extra_cycles *. cycle_time)
+                    let time_elapsed_in_cycle' =
+                      time_elapsed_in_cycle +. (duration_ms /. 1_000_000.)
                     in
 
-                    if extra_cycles <= n - 1 then
-                      loop (n - 1 - extra_cycles) time_elapsed_in_next_cycle
+                    if time_elapsed_in_cycle' > cycle_time then (
+                      let extra_cycles_time =
+                        time_elapsed_in_cycle' -. cycle_time
+                    in
+                      let extra_cycles =
+                        extra_cycles_time /. cycle_time |> ceil |> int_of_float
+                      in
+                      let time_elapsed_in_next_cycle =
+                        extra_cycles_time -.
+                          (float_of_int extra_cycles *. cycle_time)
+                      in
+
+                      if extra_cycles <= n - 1 then
+                        loop (n - 1 - extra_cycles) time_elapsed_in_next_cycle
+                      else
+                        extra_cycles
+                    ) else if time_elapsed_in_cycle' = cycle_time then
+                      loop (n - 1) 0.
                     else
-                      extra_cycles
-                  ) else if time_elapsed_in_cycle' = cycle_time then
-                    loop (n - 1) 0.
+                      loop n time_elapsed_in_cycle'
+                  ) else if time_elapsed_in_cycle > 0. then
+                    1
                   else
-                    loop n time_elapsed_in_cycle'
-                ) else if time_elapsed_in_cycle > 0. then
-                  1
-                else
-                  0
-              in
+                    0
+                in
 
-              let extra_cycles = loop cycles_to_run 0. in
-              let elapsed_in_cycles =
-                float_of_int (cycles_to_run + extra_cycles) *. cycle_time
-              in
-              clock_loop (last_clock_tick +. elapsed_in_cycles)
+                let extra_cycles = loop cycles_to_run 0. in
+                let elapsed_in_cycles =
+                  float_of_int (cycles_to_run + extra_cycles) *. cycle_time
+                in
+                clock_loop (last_clock_tick +. elapsed_in_cycles)
           | Paused ->
               if elapsed_since_last_tick > cycle_time then
                 let noop_cycles =
