5966069c15425462c9141a6054458735e0a6aefd
Emmanuel Suarez Acevedo <19786004+emmanueljs1@users.noreply.github.com>
2022-01-08 15:56:42 -0400

    Instructions decoding improvements

--- lib/instruction.ml
+++ lib/instruction.ml
@@ -34,6 +34,7 @@ type opcode =
   | SkipIfRegsNotEqual of int * int
   | SetRegToReg of int * int
   | BinaryOp of int * binary_op * int
+  | SetIndex of int
 
 type instruction =
   { opcode: opcode
@@ -42,18 +43,18 @@ type instruction =
 
 let int_of_hex (hex: char) : int = Hex.to_char '0' hex |> int_of_char
 
-let addr_of_hexes (h1: char) (h2: char) (h3: char) : int =
+let int_of_hexes (h1: char) (h2: char) (h3: char) : int =
   let upper = Hex.to_char '0' h1 |> int_of_char |> (fun i -> i lsl 8) in
   let lower = Hex.to_char h2 h3 |> int_of_char in
   upper + lower
 
 let decode_instruction (byte1: char) (byte2: char) : instruction =
   let (c1, c2), (c3, c4) = Hex.of_char byte1, Hex.of_char byte2 in
-  let hex_encodings = [c1; c2; c3; c4] in
+  let hex_encodings = List.map Char.uppercase_ascii [c1; c2; c3; c4] in
   match hex_encodings, String.init 4 (List.nth hex_encodings) with
-  | _, "00e0" -> { opcode = ClearScreen; duration_ms = 109. }
+  | _, "00E0" -> { opcode = ClearScreen; duration_ms = 109. }
   | '1' :: [h1; h2; h3], _ ->
-      let addr = addr_of_hexes h1 h2 h3 in
+      let addr = int_of_hexes h1 h2 h3 in
       { opcode = Jump addr ; duration_ms = 105. }
   | ['6'; x; h1; h2], _ ->
       let reg = int_of_hex x in
@@ -63,17 +64,15 @@ let decode_instruction (byte1: char) (byte2: char) : instruction =
       let reg = int_of_hex x in
       let value = Hex.to_char h1 h2 in
       { opcode = AddValueToReg (reg, value); duration_ms = 45. }
-  | ['a'; h1; h2; h3], _ ->
-      let upper = int_of_hex h1 |> (fun i -> i lsl 8) in
-      let lower = Hex.to_char h2 h3 |> int_of_char in
-      let value = upper + lower in
+  | ['A'; h1; h2; h3], _ ->
+      let value = int_of_hexes h1 h2 h3 in
       { opcode = SetIndexToValue value; duration_ms = 55. }
-  | ['d'; x; y; h], _ ->
+  | ['D'; x; y; h], _ ->
       let vx, vy = int_of_hex x, int_of_hex y in
       let n = int_of_hex h in
       { opcode = Display (vx, vy, n); duration_ms = 22734. }
   | ['2'; h1; h2; h3], _ ->
-      let addr = addr_of_hexes h1 h2 h3 in
+      let addr = int_of_hexes h1 h2 h3 in
       { opcode = EnterSubroutine addr; duration_ms = 105. }
   | _, "00ee" ->
       { opcode = ReturnFromSubroutine; duration_ms = 105. }
